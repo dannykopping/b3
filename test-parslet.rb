ae81859ec1702956826e4b55db8537ec45fd6033
Danny Kopping <dannykopping@gmail.com>
2018-12-23 23:59:56 +0200

    Allowing for basic argument lists to be parsed (no containership in arrays, etc) Allowing for addresses

--- test-parslet.rb
+++ test-parslet.rb
@@ -10,7 +10,10 @@ include Parslet
 #
 #
 
-string = '[{iov_len=9000000001}, 90, {bob={inner="hello"}}]'
+string = <<-EOF
+8, [9,10,100], {x='y'}, 0xdeadBEEF199
+EOF
+
 # string = '[  [       1],2 , "bob", ["another \'inner\' bob", \'rad "dude"\']  3, {hello=sally, bob= [1,2,3 ]  }    ]'
 
 class Mini < Parslet::Parser
@@ -22,7 +25,7 @@ class Mini < Parslet::Parser
   rule(:array) {
     str('[') >> (
       str('[').absent? >> array_element
-    ).repeat(1).as(:elements) >> str(']')
+    ).repeat(0).as(:elements) >> str(']')
   }
   rule(:array_element) { space? >> data_structure.as(:element) >> space? >> separator? }
 
@@ -30,36 +33,47 @@ class Mini < Parslet::Parser
   rule(:object) {
     str('{') >> (
     str('{').absent? >> property
-    ).repeat(1).as(:properties) >> str('}')
+    ).repeat(0).as(:properties) >> str('}')
   }
 
   rule(:property_key) { match(/[a-zA-Z0-9_'"]/).repeat(1) }
-  rule(:property_value) { data_structure.repeat(1) }
+  rule(:property_value) { data_structure.repeat(0) }
   rule(:property) { space? >> property_key.as(:key) >> space? >> str('=') >> space? >> property_value.as(:value) >> space? }
 
-  # ints
-  rule(:integer) { match('[0-9]').repeat(1) }
+  # ints (match integers not followed by 'x' - for address)
+  rule(:integer) { match('[0-9]').repeat(1).as(:integer) >> str('x').absent? }
+
+  # addresses
+  rule(:address) { (str('0x') >> match(/[0-9a-fA-F]/).repeat(1)).as(:address) }
 
   # strings
   rule(:string) { single_quoted_string | double_quoted_string }
   rule(:double_quoted_string) {
     str('"') >> (
       str('"').absent? >> any
-    ).repeat(1).as(:string) >> str('"')
+    ).repeat(0).as(:string) >> str('"')
   }
   rule(:single_quoted_string) {
     str("'") >> (
       str("'").absent? >> any
-    ).repeat(1).as(:string) >> str("'")
+    ).repeat(0).as(:string) >> str("'")
+  }
+
+  rule(:element_list) {
+    array_element >> space? >> separator? >> space? >> array_element.maybe
   }
 
   # whitespace
   rule(:space) { match(/\s/).repeat(1) }
   rule(:space?) { match(/\s/).repeat(1).maybe }
 
-  rule(:data_structure) { array | object | integer | string }
+  rule(:data_structure) { array | object | integer | string | address }
 
-  root(:data_structure)
+  rule(:argument_list) {
+    data_structure >> space? >> separator? >> space? >> argument_list.maybe
+  }
+
+  root(:argument_list)
 end
 
 begin
