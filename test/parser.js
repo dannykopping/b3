6857efdc407f666d8fb25a129bfaf00d344f4564
Danny Kopping <dannykopping@gmail.com>
2019-01-01 22:56:10 +0200

    More tests

--- test/parser.js
+++ test/parser.js
@@ -19,6 +19,16 @@ describe('strace output parsing', function() {
       })
     });
 
+    // maybe turn this into json-schema or something similar
+    it('accurately coerces values into appropriate data type', function() {
+      const parsed = parser.parseLine(basic_line);
+      expect(typeof parsed.pid == 'number' || parsed.pid == null).to.be.true;
+      expect(parsed.syscall).to.be.a('string');
+      expect(parsed.args).to.be.an('array');
+      expect(typeof parsed.result == 'string' || typeof parsed.result == 'number').to.be.true;
+      expect(parsed.timing).to.be.a('number');
+    });
+
     it('accurately parses a given line with a PID', function() {
       const result = {
         pid: 10757,
@@ -59,6 +69,16 @@ describe('strace output parsing', function() {
     it('refuses to parse unfinished syscalls', function() {
       expect(parser.parseLine('6955  <... futex resumed> )             = -1 ETIMEDOUT (Connection timed out)')).to.eql(null);
       expect(parser.parseLine('7016  futex(0x7ff23c537108, FUTEX_WAIT_PRIVATE, 0, {tv_sec=0, tv_nsec=12399422} <unfinished ...>')).to.eql(null);
+    });
+
+    it('fails silently if debug mode is disable and given line is invalid', function() {
+      expect(parser.parseLine('...invalid...')).to.eql(null);
+    });
+
+    it('throws an exception if debug mode is enabled and given line is invalid', function() {
+      expect(function() {
+        parser.parseLine('...invalid...', {debug: true})
+      }).to.throwException();
     });
   });
 });
